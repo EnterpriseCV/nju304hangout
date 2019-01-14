136acaf895a4548a64245d64e73e49b155bbb674
zXingchu <2649258169@qq.com>
2019-01-14 23:48:35 +0800

    msg apply ifno

--- pages/message/msg.js
+++ pages/message/msg.js
@@ -1,87 +1,103 @@
 // pages/message/msg.js
+
+var sliderWidth = 96;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    navbar: ['申请', '邀请'],
-    currentTab: 0,
+    tabs: ["申请", "邀请"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
     // invitations: {},
     applications: {}
   },
-  
-  navbarTap: function (e) {
+
+  tabClick: function(e) {
     this.setData({
-      currentTab: e.currentTarget.dataset.idx
-    })
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
     var openid = "1832247";
     wx.request({
       url: 'https://nju304.xyz/activities/applications/user/' + openid,
-      success: function (res) {
-        that.setData({ applications: res.data });
+      success: function(res) {
+        that.setData({
+          applications: res.data
+        });
       },
       // url: 'https://nju304.xyz/activities/invitations/user/' + openid,
       // success: function (res) {
       //   that.setData({ invitations: res.data });
       // }
     })
-
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
+
 })
