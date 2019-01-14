c80d766e3b5e6679f16cc5885c755a473a33bcdd
zXingchu <2649258169@qq.com>
2019-01-15 03:34:43 +0800

    like thread

--- pages/user/index.js
+++ pages/user/index.js
@@ -9,10 +9,33 @@ Page({
     user: {}
   },
 
+  likeUser: function() {
+    wx.request({
+      method: 'POST',
+      url: 'https://nju304.xyz/user/' + this.data.userId + '/0',
+      success: function (res) {
+        
+      },
+      fail: function (res) { }
+    })
+  },
+
+  threadUser: function () {
+    wx.request({
+      method: 'POST',
+      url: 'https://nju304.xyz/user/' + this.data.userId + '/1',
+      success: function (res) {
+
+      },
+      fail: function (res) { }
+    })
+
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     console.log(1)
     var that = this;
     this.setData({
@@ -21,62 +44,61 @@ Page({
     console.log(this.data.actId)
     wx.request({
       url: 'https://nju304.xyz/user/' + this.data.userId,
-      success: function (res) {
+      success: function(res) {
         that.setData({
           user: res.data
         });
       },
-      fail: function (res) {
-      }
+      fail: function(res) {}
     })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
