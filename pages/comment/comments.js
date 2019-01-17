7de18814d23e6e97c1722b7a0c0087f56c7d0c11
zXingchu <2649258169@qq.com>
2019-01-17 23:55:03 +0800

    add comment modify

--- pages/comment/comments.js
+++ pages/comment/comments.js
@@ -7,18 +7,70 @@ Page({
   data: {
     actId: "",
     comments: {},
+    showAdd: true,
+    isShow: "+",
+    sliderColor: "red"
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this;
+  sliderchange: function (event) {
+    var color = "red";
+    if (event.detail.value>=6){
+      color = "green"
+    }
     this.setData({
-      actId: options.actId
+      score: event.detail.value,
+      sliderColor: color
     });
+  },
+
+  isShowAdd: function(){
+    var show = "+";
+    if(this.data.isShow == "+"){
+      show = "-"
+    }
+    this.setData({
+      showAdd: !this.data.showAdd,
+      isShow: show
+    });
+  },
+
+  addComment: function (event) {
+    var that = this;
     wx.request({
-      url: 'https://nju304.xyz/comment/org/' + options.actId,
+      url: 'https://nju304.xyz/comment/org/' + this.data.actId,
+      method: 'POST',
+      data: {
+        userId: getApp().globalData.userInfo.openid,
+        actId: this.data.actId,
+        score: event.detail.value.slider,
+        description: event.detail.value.textarea
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: (res) => {
+        wx.showToast({
+          title: '成功',
+          icon: 'succes',
+          duration: 1000,
+          mask: true,
+          success: function () {
+            setTimeout(function () {
+              that.setData({
+                showAdd: !that.data.showAdd
+              });
+              that.getComments()
+            }, 1000);
+          }
+        });
+      }
+    })
+  },
+
+  getComments: function () {
+    var that = this;
+    wx.request({
+      url: 'https://nju304.xyz/comment/org/' + this.data.actId,
       success: function (res) {
         if (res.statusCode != 404) {
           that.setData({
@@ -30,7 +82,17 @@ Page({
 
       },
     })
+  },
 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    this.setData({
+      actId: options.actId
+    });
+    this.getComments();
   },
 
   /**
@@ -65,7 +127,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.getComments()
   },
 
   /**
